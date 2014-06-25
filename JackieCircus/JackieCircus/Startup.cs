281b9f4807e0399f10b126de998ed8cf10402162
Jacqueline Leonard <redglare@hotmail.com>
2014-06-25 16:54:32 -0400

    Build
    Start from one

---
+++ JackieCircus/JackieCircus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JackieCircus.Startup))]
+namespace JackieCircus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
